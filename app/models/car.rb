57ab34fc42697c7e8fd7d96c6c64eaa973238b79
Taylor Covington <tcovingtondev@gmail.com>
2019-09-10 19:49:45 -0600

    test all passes

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,31 +4,34 @@ class Car < ApplicationRecord
   validates :make, uniqueness: true, presence: true
   # return all cars asc order by model
   def self.by_model
-    
+    order(:model)
   end
   
   # return all cars asc order by make
   def self.by_make
-
+    order(:make)
   end
 
   # if no params passed, it'll go with default param == return asc all the car by mileage
   # :desc order by mileage
   def self.by_mileage(direction = :asc)
-
+    # Car.order(mileage: :asc)
+    # Car.order(mileage: :desc)
+    # since we already have params and default params
+    order(mileage: direction)
   end
 
   # change the color of car with the param
   def paint(color)
-
+    self.update(color: color)
   end
 # return a hash of all the cars info, except id and created/update at
   def info
-
+    self.attributes.except('id', 'created_at', 'updated_at')
   end
 # returns a string of the car horn
   def honk
-
+    'Loud!!!'
   end
 end
 # we need to test to make sure it behaves these ways
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -32,6 +32,58 @@ describe 'validations' do
   it { should validate_presence_of(:make)}
 end
 
+describe 'class methods' do
+  before(:each) do
+    @car1 = Car.create(make: 'Tesla', model: 'TZ', mileage: 0 )
+    @car2 = Car.create(make: 'Honda', model: 'Accord', mileage: 1234 )
+    @car3 = Car.create(make: 'Kia', model: 'Optima', mileage: 145000 )
+  end
+
+  it 'should sort by make :asc' do
+    expect(Car.by_make).to eq([@car2, @car3, @car1])
+  end
+
+  it 'should sort by model :asc' do
+    expect(Car.by_model).to eq([@car2, @car3, @car1])
+  end
+
+  it 'should sort by mileage :asc' do
+    expect(Car.by_mileage).to eq([@car1, @car2, @car3])
+  end
+
+  it 'should sort by mileage :desc' do
+    expect(Car.by_mileage(:desc)).to eq([@car3, @car2, @car1])
+    # we have to add :desc to give it a param or else itll be defauls
+  end
+end
+
+describe 'instance methods' do
+  before(:each) do
+    @attr = {
+      make: 'Toyota',
+      model: 'Prius',
+      color: 'Purple',
+      mileage: 100000,
+      wheels: true,
+      year: 2012
+    }
+
+    @car = Car.create(@attr)
+  end
+  it 'should paint the car' do
+    @car.paint('Blue')
+    expect(@car.color).to eq('Blue')
+  end
+
+  it 'should return the cars info' do
+    expect(@car.info).to eq(@attr.with_indifferent_access)
+  end
+
+  it 'should honk' do
+    expect(@car.honk).to eq('Loud!!!')
+  end
+end
+
 end
 
 #
